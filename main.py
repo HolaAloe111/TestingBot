44c9e3c947385c5341c1959b7d2249b8b82b494d
HolaAloe111 <dima.ezh21@gmail.com>
2025-05-26 23:39:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, ReplyKeyboardMarkup
+from aiogram.filters import Command, CommandStart
+
+bot = Bot('7540534218:AAGU_nCv162Qw2Wli5nLYa_awpV-RTp5FHk',
+          default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher(storage=MemoryStorage())
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+@dp.message(CommandStart)
+async def startmessage(message: Message):
+    await message.answer('Привет! Предлагаю тебе начать проходить тесты. Выбери одну из кнопок ниже!')
+
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt as e:
+        logging.info('Succesfull exit')
+
